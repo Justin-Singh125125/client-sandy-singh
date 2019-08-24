b9c30ddec207d2e82225b8dd754e1d63bf4e3d6c
Justin Singh <justin.singh125125@gmail.com>
2019-08-23 19:43:07 -0700

    added favicon

---
+++ build/precache-manifest.6c4327453331a75a0b8fa02b896f4ae1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e5ddedb0e7d66df50009f4d7392fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3981313ab24680a4cc93",
+    "url": "/static/css/main.6b047fcf.chunk.css"
+  },
+  {
+    "revision": "2367d541f227fc535047",
+    "url": "/static/js/2.af526473.chunk.js"
+  },
+  {
+    "revision": "3981313ab24680a4cc93",
+    "url": "/static/js/main.b72f6b13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1940efd7a909de8111ede733a7875f78",
+    "url": "/static/media/gallery-1.1940efd7.jpg"
+  },
+  {
+    "revision": "8763e2cf7555bf25e23a22d58fd96145",
+    "url": "/static/media/gallery-10.8763e2cf.jpg"
+  },
+  {
+    "revision": "8a8caaebed102e5d5039ee4e883b1398",
+    "url": "/static/media/gallery-11.8a8caaeb.jpg"
+  },
+  {
+    "revision": "c495d2b793b961cd459426c686a9e2a6",
+    "url": "/static/media/gallery-12.c495d2b7.jpg"
+  },
+  {
+    "revision": "b8d64498b6f4f5d8d0a5bf80094f9e9a",
+    "url": "/static/media/gallery-13.b8d64498.jpg"
+  },
+  {
+    "revision": "72eba670d24232dc60a6572b00274559",
+    "url": "/static/media/gallery-14.72eba670.jpg"
+  },
+  {
+    "revision": "5f6ffcada019439718793a2c55bfa73f",
+    "url": "/static/media/gallery-2.5f6ffcad.jpg"
+  },
+  {
+    "revision": "3b2166966ebc27339d90333b92806c58",
+    "url": "/static/media/gallery-3.3b216696.jpg"
+  },
+  {
+    "revision": "c4daff59002b242cf70f4510f5c917cb",
+    "url": "/static/media/gallery-4.c4daff59.jpg"
+  },
+  {
+    "revision": "a57ac72d47ed56f3e07f775493ca3f39",
+    "url": "/static/media/gallery-5.a57ac72d.jpg"
+  },
+  {
+    "revision": "0933bac36836e21b76b256b460f0e26c",
+    "url": "/static/media/gallery-6.0933bac3.jpg"
+  },
+  {
+    "revision": "8d9a55a8270ec9be4840fdaae1e71463",
+    "url": "/static/media/gallery-7.8d9a55a8.jpg"
+  },
+  {
+    "revision": "6faab652c8eb3a0e9e3fbeb6def0196a",
+    "url": "/static/media/gallery-8.6faab652.jpg"
+  },
+  {
+    "revision": "6ccb0105d7898fce12d48cca20bd8185",
+    "url": "/static/media/gallery-9.6ccb0105.jpg"
+  },
+  {
+    "revision": "023f99915191e2cfa44ef9b782a0543d",
+    "url": "/static/media/hero-2.023f9991.jpg"
+  },
+  {
+    "revision": "dd186e4d451d79c1b99bd105c5748092",
+    "url": "/static/media/sprite.dd186e4d.svg"
+  },
+  {
+    "revision": "0f5634d010049832d31c7455c92588b8",
+    "url": "/static/media/testimonial-pic--1.0f5634d0.jpg"
+  }
+]);
